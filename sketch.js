6d8193b199543e7050cc01049a6b9433f0980981
dmarie777 <deimarie777@gmail.com>
2024-07-17 22:48:19 -0500

    Refactored codebase to use OOP

--- sketch.js
+++ sketch.js
@@ -2,114 +2,151 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
 
-const canvas = document.querySelector('.webgl')
-const hdrTextureURL = './static/textures/environmentMap/2k.hdr'
+//Textures
+const doorColorTexture = './static/textures/door/color.jpg';
+const doorAlphaTexture = './static/textures/door/alpha.jpg';
+const doorAmbientOclusionTexture = './static/textures/door/ambientOcclusion.jpg';
+const doorHeightTexture = './static/textures/door/height.jpg';
+const doorNormalTexture = './static/textures/door/normal.jpg';
+const doorRoughnessTexture = './static/textures/door/roughness.jpg';
+const matcapTexture = './static/textures/matcaps/1.png';
+const gradientTexture = './static/textures/door/3.jpg';
 
-const size = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+const hdrTextureURL = './static/textures/environmentMap/2k.hdr';
 
-//Scene
-const scene = new THREE.Scene();
+export default class sketch {
+    constructor() {
 
-//Texture
-const textureLoader = new THREE.TextureLoader()
-const doorColorTexture = textureLoader.load('./static/textures/door/color.jpg')
-const doorAlphaTexture = textureLoader.load('./static/textures/door/alpha.jpg')
-const doorAmbientOclusionTexture = textureLoader.load('./static/textures/door/ambientOcclusion.jpg')
-const doorHeightTexture = textureLoader.load('./static/textures/door/height.jpg')
-const doorNormalTexture = textureLoader.load('./static/textures/door/normal.jpg')
-const doorRoughnessTexture = textureLoader.load('./static/textures/door/roughness.jpg')
-const matcapTexture = textureLoader.load('./static/textures/matcaps/1.png')
-const gradientTexture = textureLoader.load('./static/textures/door/3.jpg')
+        //sizes
+        this.height = window.innerHeight;
+        this.width = window.innerWidth;
 
-//Color space
-doorColorTexture.colorSpace = THREE.SRGBColorSpace
+        //camera
+        this.camera = new THREE.PerspectiveCamera(75, this.width / this.height, 0.1, 100);
+        this.camera.position.z = 1
 
-//HDR Texture
-const loader = new RGBELoader();
-loader.load(hdrTextureURL, function(texture) {
-    texture.mapping = THREE.EquirectangularReflectionMapping;
-    scene.background = texture;
-} )
+        //Scene
+        this.scene = new THREE.Scene();
 
-//Material
-const basicMaterial = new THREE.MeshBasicMaterial(
-    {
-        map:doorColorTexture,
-        side: THREE.DoubleSide,
+        //renderer
+        this.canvas = document.querySelector('.webgl');
+        this.renderer = new THREE.WebGLRenderer(
+            {
+                canvas: this.canvas
+            }
+        )
+        this.renderer.setSize( this.width, this.height );
+
+        //Controls
+        this.controls = new OrbitControls( this.camera, this.renderer.domElement );
+        this.controls.enableDamping = true;
+
+        //Texture loaders
+        this.textureLoader = new THREE.TextureLoader();
+        this.RGBELoader = new RGBELoader();
+
+        //settings
+        this.resize();
+
+        //Background
+        this.addBackground();
+        this.postProcessing();
+
+        //Add objects
+        this.addPlane();
+        this.addSphere();
+        this.addTorus();
+
+        //render
+        this.clock = new THREE.Clock;
+        this.animate();
+
     }
-)
 
-//Objects
-const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 16, 16),
-    basicMaterial
-)
-const plane = new THREE.Mesh(
-    new THREE.PlaneGeometry(1,1),
-    basicMaterial
-)
-plane.position.x = 1.5;
-const torus = new THREE.Mesh(
-    new THREE.TorusGeometry(0.3, .2, 16, 34),
-    basicMaterial
-)
-torus.position.x = -1.5;
+    resize() {
+        window.addEventListener('resize', () => {
+        //Update sizes
+        this.width = window.innerWidth
+        this.height = window.innerHeight
 
-scene.add(sphere, plane, torus);
+        //Update camera
+        this.camera.aspect = this.width / this.height
+        this.camera.updateProjectionMatrix()
 
-//create camera
-const camera = new THREE.PerspectiveCamera(75, size.width / size.height, 0.1, 100)
-camera.position.z = 2
-scene.add(camera)
-
-//Add renderer
-const renderer = new THREE.WebGLRenderer(
-    {
-        canvas: canvas
-    }
-)
-
-renderer.outputEncoding = THREE.sRGBEncoding;
-renderer.toneMapping = THREE.ACESFilmicToneMapping;
-renderer.toneMappingExposure = 1.5;
-renderer.setSize(size.width, size.height)
-
-//resize event
-window.addEventListener('resize', () => {
-    //Update sizes
-    size.width = window.innerWidth
-    size.height = window.innerHeight
-
-    //Update camera
-    camera.aspect = size.width / size.height
-    camera.updateProjectionMatrix()
-
-    //Update renderer
-    renderer.setSize(size.width, size.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+        //Update renderer
+        this.renderer.setSize(this.width, this.height)
+        this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
+    }
 
-//Add orbit controls
-const controls = new OrbitControls( camera, renderer.domElement )
-controls.enableDamping = true
+    addBackground() {
+        // Load HDR Texture for the background
+        this.RGBELoader.load(hdrTextureURL, function(texture) {
+            texture.mapping = THREE.EquirectangularReflectionMapping;
+            this.scene.background = texture;
+        }.bind(this) )
+    }
 
-const clock = new THREE.Clock;
+    postProcessing() {
+        this.renderer.outputEncoding = THREE.sRGBEncoding;
+        this.renderer.toneMapping = THREE.ACESFilmicToneMapping;
+        this.renderer.toneMappingExposure = 1.5;
+    }
 
-function animate() {
-    requestAnimationFrame(animate)
+    addPlane() {
+        //load texture
+        const t1 = this.textureLoader.load(doorColorTexture);
+        // Color space
+        t1.colorSpace = THREE.SRGBColorSpace;
+        //create material
+        this.basicMaterial = new THREE.MeshBasicMaterial(
+            {
+                map:t1,
+                side: THREE.DoubleSide,
+            }
+        )
+        //create mesh
+        this.plane = new THREE.Mesh(
+            new THREE.PlaneGeometry(1,1),
+            this.basicMaterial
+        )
+        this.plane.position.x = 1.5;
+        this.scene.add( this.plane)
+    }
 
-    const elapsedTime = clock.getElapsedTime();
-    //Animations
-    plane.rotation.y = 0.5 * elapsedTime;
-    torus.rotation.y = 0.5 * elapsedTime;
-    sphere.rotation.y = 0.5 * elapsedTime;
+    addSphere() {
 
-    controls.update()
-    renderer.render(scene, camera)
+        this.sphere = new THREE.Mesh(
+            new THREE.SphereGeometry(0.5, 16, 16),
+            new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+        )
+        this.scene.add( this.sphere);
+    }
+
+    addTorus() {
+
+        this.torus = new THREE.Mesh(
+            new THREE.TorusGeometry(0.3, .2, 16, 34),
+            new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+        )
+        this.torus.position.x = -1.5;
+        this.scene.add( this.torus);
+
+    }
+
+    animate() {
+        window.requestAnimationFrame(this.animate.bind(this));
+
+        const elapsedTime = this.clock.getElapsedTime();
+        //Animations
+        this.plane.rotation.y = 0.5 * elapsedTime;
+        this.torus.rotation.y = 0.5 * elapsedTime;
+        this.sphere.rotation.y = 0.5 * elapsedTime;
+
+        this.controls.update();
+        this.renderer.render(this.scene, this.camera);
+    }
+
 }
 
-animate()
-
+new sketch()
